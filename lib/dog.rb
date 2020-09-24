536e63187f5bea3c4c249df1935f3cbbbe02629d
Rosie Choi <rosiechoimanso@gmail.com>
2020-09-24 04:47:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,13 @@
 # Your code goes here!
 class Dog 
-  def name =(dogs_name)
-    @name = (dogs_name)
+   def name=(dogs_name)
+    @this_dogs_name = dogs_name
   end
+ 
   def name
-    @name
+    @this_dogs_name
   end
+  
   def bark
     puts "woof!"
   end
